ef2331750ef39b05720e8a3b585ad00b6891bdf9
radityawibhawa <radityawibhawa13@gmail.com>
2024-04-28 20:52:34 +0800

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import AppRoute from './AppRoute.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 
